621780b0956bf87e628e23a32b59669c87ea4992
juanmarianofajardo-blip <juanmarianofajardo@gmail.com>
2025-08-31 21:03:25 +0800

    index.html

--- file/love.js
+++ file/love.js
@@ -75,7 +75,7 @@
         this.tree = tree;
 
         var scale = scale || 1
-        var color = '#FFC0CB';
+        var color = '#75df67ff';
 
         this.heart = {
             point  : point,
@@ -169,7 +169,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("no jumpscare mhm", 28, 10);
             ctx.restore();
         },
         clear: function() {
