00cd6984f1d0af6b2ffb45dfbb107eb436b85b6f
juanmarianofajardo-blip <juanmarianofajardo@gmail.com>
2025-08-31 21:12:56 +0800

    bye china

--- file/love.js
+++ file/love.js
@@ -167,7 +167,7 @@
 
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
-            ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
+            ctx.font = "12px,Verdana"; 
             ctx.fillText("Click Me:) ", 30, -5);
             ctx.fillText("no jumpscare mhm", 28, 10);
             ctx.restore();
